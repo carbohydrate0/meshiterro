308552a068b32875ad8168255f8579337b933db2
carbohydrate0 <e6a201052d@edu.teu.ac.jp>
2021-09-16 05:52:40 +0000

    [Finish]3-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '58b96783b3397ca109c2ea5a68a625465a7a429bb97bcaece7714ed4374d21fac036b4331974198ce87525eb7996da4ae3c485f486dda2187b5f16a0c8ac1441'
